9088ea77437c1fd33e997c61e82edc4fed98f577
Steven Kneisler <stevenkneisler@gmail.com>
2019-04-22 23:43:56 -0400

    Use generic values for config and servers

--- src/sql/initServers.sql
+++ src/sql/initServers.sql
@@ -3,5 +3,5 @@ DELETE FROM servers WHERE 1=1;
 INSERT INTO
     servers (name, url, port, access_level)
 VALUES
-    ('Main', 'game.yogstation.net', 4133, 'ALL'),
-    ('Test', 'whatever', 1234, 'ADMIN');
+    ('Main', 'main.example.com', 4133, 'ALL'),
+    ('Test', 'test.example.com', 1234, 'ADMIN');
--- src/sql/setConfig.sql
+++ src/sql/setConfig.sql
@@ -3,15 +3,15 @@ DELETE FROM config WHERE 1=1;
 INSERT INTO
     config (cfg_key, cfg_value)
 VALUES
-    ('community_name', 'Yogstation'),
-    ('forums_url', 'https://forums.yogstation.net/index.php'),
-    ('github_url', 'https://github.com/yogstation13/yogstation'),
-    ('wiki_url', 'https://wiki.yogstation.net/'),
+    ('community_name', 'My SS13 Community'),
+    ('forums_url', 'https://example.com/forums'),
+    ('github_url', 'https://example.com/github'),
+    ('wiki_url', 'https://example.com/wiki'),
     ('footer_text', 'CentCom - SS13 Management Platform'),
-    ('panel_header_text', 'Yogstation 13'),
-    ('splash_title_text', 'Yogstation 13'),
-    ('panel_home_intro_text', 'Yogstation 13'),
-    ('twitter_url', 'https://twitter.com/Yogstation_13'),
-    ('steam_url', 'https://steamcommunity.com/groups/yogstation13'),
-    ('discord_url', 'https://discord.gg/0keg6hQH05Ha8OfO'),
-    ('donate_url', 'https://www.yogstation.net/index.php?do=donate');
+    ('panel_header_text', 'My SS13 Community'),
+    ('splash_title_text', 'My SS13 Community'),
+    ('panel_home_intro_text', 'My SS13 Community'),
+    ('twitter_url', 'https://example.com/twitter'),
+    ('steam_url', 'https://example.com/steam'),
+    ('discord_url', 'https://example.com/discord'),
+    ('donate_url', 'https://example.com/donate');
