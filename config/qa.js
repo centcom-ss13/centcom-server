56a46343c384e22d36e712b7fdbe4921e11013ff
Steven Kneisler <stevenkneisler@gmail.com>
2019-04-22 20:00:28 -0400

    Set more qa config

--- config/qa.js
+++ config/qa.js
@@ -2,5 +2,6 @@ module.exports = {
   databaseUrl: '192.168.1.10',
   databaseUsername: undefined, //go get this somewhere else
   databasePassword: undefined,
-  apiPort: 80,
+  apiHost: 'centcom.ddmers.com',
+  apiPort: 3000,
 };
