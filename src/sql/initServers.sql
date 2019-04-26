91cbc03553bfb62f2fd5ace5f85a0ea78137734c
Steven Kneisler <stevenkneisler@gmail.com>
2019-04-26 01:38:45 -0400

    Remove deprecated sql directory and add db:migrate scripts

--- src/sql/initServers.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM servers WHERE 1=1;
-
-INSERT INTO
-    servers (name, url, port, access_level)
-VALUES
-    ('Main', 'main.example.com', 4133, 'ALL'),
-    ('Test', 'test.example.com', 1234, 'ADMIN');
