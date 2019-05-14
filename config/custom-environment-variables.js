c166c0949162b21aeaf99d7f4b811529cd5cbbd7
Steven Kneisler <stevenkneisler@gmail.com>
2019-05-14 01:12:07 -0400

    Move config to @centcom/core

--- config/custom-environment-variables.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  databaseUrl: 'CENTCOM_DB_HOST',
-  databaseUsername: 'CENTCOM_DB_USER',
-  databasePassword: 'CENTCOM_DB_PASSWORD',
-  apiSSLKeyFile: 'CENTCOM_SSL_KEY_FILE',
-  apiSSLCertFile: 'CENTCOM_SSL_CERT_FILE',
-  apiSSLKeyPassphrase: 'CENTCOM_SSL_KEY_PASSPHRASE',
-};
